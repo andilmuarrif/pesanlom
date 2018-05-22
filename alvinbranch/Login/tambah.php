b8e1728da5044b63fb52ad7af43c0d04607ddc7e
alvinalvandi <37974654+alvinalvandi@users.noreply.github.com>
2018-05-22 23:31:28 +0700

    Update tambah.php

--- alvinbranch/Login/tambah.php
+++ alvinbranch/Login/tambah.php
@@ -1,17 +1,26 @@
-<?php
+<?php 
 //koneksi ke database
 require 'functionedit.php';
 
 
-//ambil data dari tabel barang
-
-$queryy = query("SELECT * FROM barang ");
-
-
-/** proses pengecekan apakah data dari database berhasil ditampilkan atau 
-tidak dengan menggunakan tampilan var_dump
-while ($data = mysqli_fetch_assoc($query) ) {
-*var_dump($data); }
-*/
-
+//cek apakah tombol submit sudah ditekan atau belom
+if( isset($_POST["submit"]) ){
+   if( tambah($_POST) > 0 ) {
+       echo "
+            <script>
+                alert('data berhasil ditambahkan!');
+                document.location.href = '../Home/addbarang.php'
+            </script>
+       ";
+   } else {
+       echo "
+            <script>
+                alert('data gagal ditambahkan!');
+                document.location.href = '../Home/addbarang.php'
+            </script>
+       ";
+   }
+    
+   
+}
 ?>
