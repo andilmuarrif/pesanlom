28a0bdd679b1d59a63e9ad14cfabcc3790607bb9
alvinalvandi <37974654+alvinalvandi@users.noreply.github.com>
2018-05-22 23:09:49 +0700

    Update tambah.php

--- alvinbranch/Login/tambah.php
+++ alvinbranch/Login/tambah.php
@@ -1,35 +1,17 @@
-<?php 
+<?php
 //koneksi ke database
-$conn = mysqli_connect("localhost", "root", "", "pesanlom");
+require 'functionedit.php';
 
 
-//cek apakah tombol submit sudah ditekan atau belom
-if( isset($_POST["submit"]) ){
-    //ambil data dari tiap elemen dalam form
-    $nama_barang = htmlspecialchars($_POST["nama_barang"]);
-    $harga = htmlspecialchars($_POST["harga"]);
-    $stok = htmlspecialchars($_POST["stok"]);
-    
-    //query insert data
-    $query = "INSERT INTO barang 
-                VALUES('$nama_barang', '$harga', '$stok')";
-    mysqli_query($conn, $query);
+//ambil data dari tabel barang
 
-    //cek apakah data berhasil ditambahkan atau tidak
-    if( mysqli_affected_rows($conn) > 0 ) {
-        echo "
-            <script>
-                alert('data berhasil ditambahkan!');
-                document.location.href = '../home/addbarang.php';
-            </script>
-        ";
-    } else {
-        echo "<script>
-                alert('data gagal ditambahkan!');
-                document.location.href = '../home/addbarang.php';
-            </script> 
-        ";
-    } 
-    
-}
+$queryy = query("SELECT * FROM barang ");
+
+
+/** proses pengecekan apakah data dari database berhasil ditampilkan atau 
+tidak dengan menggunakan tampilan var_dump
+while ($data = mysqli_fetch_assoc($query) ) {
+*var_dump($data); }
+*/
+
 ?>
