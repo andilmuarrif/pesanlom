c2f0812b3e9e88f67f290ee579cdefe4bb58a252
muhammadalmuhtadi <37921765+muhammadalmuhtadi@users.noreply.github.com>
2018-05-24 21:54:18 +0700

    Add files via upload

--- albranch2/connect.php
+++ albranch2/connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbName = "db_tutorial";
+$dbName = "pesanlom";
 mysql_connect($host, $user, $pass);
 mysql_select_db($dbName)
 or die ("Connect Failed !! : ".mysql_error());
