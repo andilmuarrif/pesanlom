963319e00b85d9def51d7fed7aa9da2da751cf3b
Pilar Al Hafist <38108355+pilaralhafist@users.noreply.github.com>
2018-05-24 23:51:30 +0700

    Update logout.php

--- pilarbranch2/Login/logout.php
+++ pilarbranch2/Login/logout.php
@@ -1,6 +1,6 @@
 <?php   
 session_start(); 
 session_destroy(); 
-header("location:../Home/Home.html"); //logout dan kembali ke halaman awal
+header("location:../Home/index.php"); //logout dan kembali ke halaman index
 exit();
 ?>
