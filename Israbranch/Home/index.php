f7ea00bc5acf441b582f8aa6c05ab6041f0f4acc
israfarliadi <38126194+israfarliadi@users.noreply.github.com>
2018-05-24 14:29:40 +0700

    Add files via upload

--- Israbranch/Home/index.php
+++ Israbranch/Home/index.php
@@ -4,7 +4,7 @@
     <title>PesanLom</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<!--index merupakan halaman awal dari program web-->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900|Raleway" rel="stylesheet">
 
     <link rel="stylesheet" href="css/bootstrap.css">
@@ -35,10 +35,7 @@
                 <a class="nav-link" href="../login/loginindex.html">			  
 				<?php
 				session_start();
-				if ($_SESSION == true)
 				echo 'Helo '.$_SESSION['username'];
-				else
-				echo 'login';
 			?></a>
               </li>
 			  <li class="nav-item">
@@ -62,11 +59,27 @@
               <h1>PesanLom</h1>
               <p>Bonding Companies all Around</p>
             </div>
-
-            <form class="form-inline element-animate" id="search-form">
-              <input type="text" class="form-control form-control-block search-input" id="autocomplete" placeholder="Search Product" onFocus="geolocate()">
-              <button type="submit" class="btn btn-primary">Search</button>
+<!--
+            <form name="formcari" method="post" action="search_exe.php">
+<table width="330" border="0" align="center" cellpadding="0">
+<tr bgcolor="orange">
+<td height="25" colspan="3">
+<strong> Student Searching ! </strong>
+</td>
+</tr>
+<tr> <td>  Name </td>
+<td> <input type="text" name="nama_barang"> </td>
+</tr>
+<td></td>
+<td> <input type="SUBMIT" name="SUBMIT" id="SUBMIT" value="search" > </td>
+</table>
+</form>
+    -->        
+            <form method="post" action="home.php" class="form-inline element-animate" id="search-form">
+              <input type="text" name="nama_barang" class="form-control form-control-block search-input" id="autocomplete" placeholder="Search Product" onFocus="geolocate()">
+              <button type="submit" name="submit" id="submit" class="btn btn-primary">Search</button>
             </form>
+            
 
           </div>
         </div>
@@ -99,11 +112,9 @@
     <script>
       // This example displays an address form, using the autocomplete feature
       // of the Google Places API to help users fill in the information.
-
       // This example requires the Places library. Include the libraries=places
       // parameter when you first load the API. For example:
       // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
       var placeSearch, autocomplete;
       var componentForm = {
         street_number: 'short_name',
@@ -113,28 +124,23 @@
         country: 'long_name',
         postal_code: 'short_name'
       };
-
       function initAutocomplete() {
         // Create the autocomplete object, restricting the search to geographical
         // location types.
         autocomplete = new google.maps.places.Autocomplete(
             /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
             {types: ['geocode']});
-
         // When the user selects an address from the dropdown, populate the address
         // fields in the form.
         autocomplete.addListener('place_changed', fillInAddress);
       }
-
       function fillInAddress() {
         // Get the place details from the autocomplete object.
         var place = autocomplete.getPlace();
-
         for (var component in componentForm) {
           document.getElementById(component).value = '';
           document.getElementById(component).disabled = false;
         }
-
         // Get each component of the address from the place details
         // and fill the corresponding field on the form.
         for (var i = 0; i < place.address_components.length; i++) {
@@ -145,7 +151,6 @@
           }
         }
       }
-
       // Bias the autocomplete object to the user's geographical location,
       // as supplied by the browser's 'navigator.geolocation' object.
       function geolocate() {
